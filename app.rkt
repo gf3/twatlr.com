590a00609ec471570d6d48a6d0b421a698bfbc55
Gianni Chiappetta <gianni@runlevel6.org>
2011-08-03 02:36:41 -0400

    added: Almost working concept.

--- app.rkt
+++ app.rkt
@@ -1,4 +1,52 @@
-(require (planet dyoo/string-template:1:0/string-template)
-         "vendor/twatlr")
+#lang racket
 
+(require net/url
+         web-server/http
+         web-server/dispatch
+         web-server/servlet-env
+         "vendor/twatlr/twatlr.rkt"
+         (planet dyoo/string-template:1:0/string-template))
+
+(define-values (twatlr-dispatch twatlr-url)
+    (dispatch-rules
+     [("") home-page]
+     [("thread" (string-arg)) view-thread]))
+
+(define (home-page req)
+  (render home-page-tmpl))
+(define (view-thread req t)
+  (response/xexpr
+    `(html (head (title "View Thread"))
+           (body (p "View thread for tweet:")
+                 (p ,t)))))
+(define (not-found req)
+  (response/xexpr
+    `(html (head (title "Hello world!"))
+           (body (p "Hey out there!")))))
+
+(define-values (home-page-tmpl) (file->string "./home-page.html"))
+
+(define (render content)
+  (response/full
+    200 #"Okay"
+    (current-seconds) TEXT/HTML-MIME-TYPE
+    (list (make-header #"Content-Length" (string->bytes/utf-8 (number->string (string-length content)))))
+    (list (string->bytes/utf-8 content))))
+
+(define (url->request u)
+  (make-request #"GET" (string->url u) empty
+                (delay empty) #f "1.2.3.4" 80 "4.3.2.1"))
+
+; (write (twatlr-dispatch
+;    (url->request "http://gf3.ca")))
+; 
+; (write (twatlr-dispatch
+;    (url->request "http://gf3.ca/thread/1234abcd")))
+
+; (serve/dispatch twatlr-dispatch)
+
+(serve/servlet twatlr-dispatch
+  #:extra-files-paths (list (build-path "./public"))
+  #:log-file (build-path "./log/app.log")
+  #:servlet-regexp #rx"")
 
