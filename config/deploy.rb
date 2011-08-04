77cd34eb38cd0841ede0e7e9ded51de8fc0aa9bb
Gianni Chiappetta <gianni@runlevel6.org>
2011-08-04 11:37:34 -0400

    updated: Switch production server.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,12 +2,12 @@ set :application, "twatlr"
 set :repository,  "git://github.com/gf3/twatlr.com.git"
 set :deploy_to,   "/data/www/twatlr.com"
 set :scm,         :git
-set :user,        :gianni
+set :user,        :apprunner
 
 set :use_sudo,    false
 
-role :web, "192.168.10.10"                          # Your HTTP server, Apache/etc
-role :app, "192.168.10.10"                          # This may be the same as your `Web` server
+role :web, "173.255.226.20"   # Your HTTP server, Apache/etc
+role :app, "173.255.226.20"   # This may be the same as your `Web` server
 
 namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
