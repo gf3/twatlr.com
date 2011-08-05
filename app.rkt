1024f9dc7b9c5a260d01d677ecbb0ec3acbe3317
Gianni Chiappetta <gianni@runlevel6.org>
2011-08-05 16:00:50 -0400

    fixed: Fixed linkify-url.

--- app.rkt
+++ app.rkt
@@ -100,7 +100,7 @@
 
 ; Linkfiy all URLs in a string
 (define (linkify-url text)
-  (let ([r-http #px"$[a-z]://"] [r-link #px"\\b(?:[a-z]+://)?(?<!@)[0-9a-z](?:[-\\d\\w.]*\\.)+[a-z]{2,4}(?:\\:\\d{1,6})?(?:[-\\d\\w./?=&#%+]*)\\b"])
+  (let ([r-http #px"^[a-z]+://"] [r-link #px"\\b(?:[a-z]+://)?(?<!@)[0-9a-z](?:[-\\d\\w.]*\\.)+[a-z]{2,4}(?:\\:\\d{1,6})?(?:[-\\d\\w./?=&#%+]*)\\b"])
     (regexp-replace* r-link text (λ (url)
                                   (let ([fixed-url (if (regexp-match? r-http url) url (string-append "http://" url))])
                                     (string-append "<a href=" fixed-url ">" url "</a>"))))))
