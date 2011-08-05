543e5b536e710232d564a1c7e43398502b97afdd
Gianni Chiappetta <gianni@runlevel6.org>
2011-08-05 12:35:22 -0400

    update: LOL SUDO.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,15 +4,15 @@ set :deploy_to,   "/data/www/twatlr.com"
 set :scm,         :git
 set :user,        :apprunner
 
-set :use_sudo,    false
-
 role :web, "173.255.226.205"   # Your HTTP server, Apache/etc
 role :app, "173.255.226.205"   # This may be the same as your `Web` server
+
+default_run_options[:pty] = true
 
 namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "cd #{release_path}; git submodule init; git submodule update"
-    run "god restart twatlr"
+    sudo "god restart twatlr"
   end
 end
 
