7c12aa8465fe8772c35c17904c721cff45803bcb
Gianni Chiappetta <gianni@runlevel6.org>
2012-03-02 03:24:13 -0500

    Have Capistrano take care fo submodules

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,16 +4,18 @@ set :deploy_to,   "/var/sites/twatlr.com"
 set :scm,         :git
 set :user,        :gianni
 
+set :git_enable_submodules, 1
+
 role :web, "46.38.167.162"
 role :app, "46.38.167.162"
 
-default_run_options[:pty] = true
+# default_run_options[:pty] = true
 
 namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "cd #{release_path}; git submodule init; git submodule update"
+    # run "cd #{release_path}; git submodule init; git submodule update"
     run "kill $(ps -C app.rkt -o pid=); true" # Ignore failed kills
-    run "nohup racket #{release_path}/app.rkt > /dev/null 2>&1 &" # Start server
+    run "nohup /usr/bin/racket #{release_path}/app.rkt > /dev/null 2>&1 &" # Start server
   end
 end
 
