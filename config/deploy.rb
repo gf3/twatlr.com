bbcec20a4f46b046638ce9f7511933999fd38558
Gianni Chiappetta <gianni@runlevel6.org>
2012-12-19 17:35:49 -0500

    Update deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :application, "twatlr"
 set :repository,  "git://github.com/gf3/twatlr.com.git"
-set :deploy_to,   "/var/www/twatlr.com"
+set :deploy_to,   "/usr/local/www/gianni/twatlr.com"
 set :scm,         :git
 set :user,        :gianni
 
@@ -15,7 +15,7 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     # run "cd #{release_path}; git submodule init; git submodule update"
     run "kill `ps -C app.rkt -o pid=`; true" # Ignore failed kills
-    run "nohup /usr/bin/racket #{release_path}/app.rkt > /dev/null 2>&1 &" # Start server
+    run "nohup /usr/local/bin/racket #{release_path}/app.rkt > /dev/null 2>&1 &" # Start server
   end
 end
 
