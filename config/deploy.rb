e4b4ab890f2f14fa24be7dc03ccfd3c376259761
Gianni Chiappetta <gianni@runlevel6.org>
2013-01-17 08:01:32 -0800

    New server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,18 @@
-set :application, "twatlr"
-set :repository,  "git://github.com/gf3/twatlr.com.git"
-set :deploy_to,   "/usr/local/www/gianni/twatlr.com"
+set :application, 'twatlr'
+set :repository,  'git://github.com/gf3/twatlr.com.git'
+set :deploy_to,   '/usr/local/www/gianni/twatlr.com'
 set :scm,         :git
 set :user,        :gianni
 
 set :git_enable_submodules, 1
 
-role :web, "46.38.167.162"
-role :app, "46.38.167.162"
-
-# default_run_options[:pty] = true
+role :web, '208.68.37.29'
+role :app, '208.68.37.29'
 
 namespace :deploy do
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    # run "cd #{release_path}; git submodule init; git submodule update"
-    run "ps aux | grep app.rkt | awk '{print $2}' | xargs kill; true" # Ignore failed kills
-    run "nohup /usr/local/bin/racket #{release_path}/app.rkt > /dev/null 2>&1 &" # Start server
+  task :restart, roles: :app, except: { no_release: true } do
+    run 'ps aux | grep app.rkt | awk "{print $2}" | xargs kill; true'
+    run 'nohup /usr/local/bin/racket %s/app.rkt > /dev/null 2>&1 &' % release_path
   end
 end
 
